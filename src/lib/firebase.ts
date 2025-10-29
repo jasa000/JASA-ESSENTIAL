a4d02dab3c9092e00ff9fdb2c5964ceb6b26f627
JASA Essential <essentialjasa@gmail.com>
2025-10-29 09:32:25 +0000

    Replace the firebase valuees with below conf // Import the functions you

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,15 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAsWv9ZK50xQ1mSZm42dYnORoMS79aCiMY",
+  authDomain: "studio-8473300000-73d0f.firebaseapp.com",
+  projectId: "studio-8473300000-73d0f",
+  storageBucket: "studio-8473300000-73d0f.firebasestorage.app",
+  messagingSenderId: "713659587319",
+  appId: "1:713659587319:web:e9b15f7a7464f3a78051f2"
 };
 
 // Initialize Firebase
