ddbb8cace5024277038a6168d95dfd61250f004d
JASA Essential <essentialjasa@gmail.com>
2025-10-29 08:49:17 +0000

    Set as workspace get conf from .env and when deployed in netlify the app

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCNzaRhZ9GOtfiVHC4fvhSL6m7rGT7zSWg",
-  authDomain: "jasa-booking.firebaseapp.com",
-  projectId: "jasa-booking",
-  storageBucket: "jasa-booking.firebasestorage.app",
-  messagingSenderId: "588977017789",
-  appId: "1:588977017789:web:6cb32557fe40a6db9db8d4",
-  measurementId: "G-TWSSVNRRCL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
