ebc60f9d5c9a2d56b4c23778ab8c265aa90b3aae
JASA Essential <essentialjasa@gmail.com>
2025-10-31 08:41:08 +0000

    Add a home icon near menu icon in header used to redirect to home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { Bell, LogIn, LogOut, Search, ShoppingCart, User } from 'lucide-react';
+import { Bell, LogIn, LogOut, Search, ShoppingCart, User, Home } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import Link from 'next/link';
 import { Button } from './ui/button';
@@ -43,6 +43,12 @@ export default function Header() {
       <div className="container mx-auto flex h-20 items-center justify-between px-4">
         <div className="flex items-center gap-4">
             <SidebarTrigger className="relative h-7 w-7 bg-transparent text-foreground hover:bg-transparent/20" />
+            <Button asChild variant="ghost" size="icon" className='h-7 w-7'>
+              <Link href="/">
+                  <Home className="h-5 w-5" />
+                  <span className="sr-only">Home</span>
+              </Link>
+            </Button>
             <Link href="/" className="hidden items-center gap-2 sm:flex">
                 <span className="font-headline text-lg font-bold">
                 Jasa Essentials
