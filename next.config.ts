fe3773a3e1280c2c813960fe901caaf2d0ef6449
JASA Essential <jasaessential3@gmail.com>
2025-12-03 22:14:27 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 
-import type {NextConfig} from 'next';
-
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -60,4 +58,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
